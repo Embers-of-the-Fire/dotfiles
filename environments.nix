f7a96b72097ff4b832314281dc2e908a01148c3d
Embers-of-the-Fire <stellarishs@163.com>
2026-04-09 17:02:47 +0800

    sync

--- environments.nix
+++ environments.nix
@@ -17,5 +17,10 @@
     HTTPS_PROXY = "http://localhost:7897";
     ALL_PROXY = "socks5://localhost:7897";
     NO_PROXY = "localhost,127.0.0.1";
+
+    # NVIDIA Optimus
+    __NV_PRIME_RENDER_OFFLOAD = "1";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    __VK_LAYER_NV_optimus = "NVIDIA_only";
   };
 }
